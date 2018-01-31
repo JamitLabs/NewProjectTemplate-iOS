aa3980a5f6fe5ef5bf9275867e5bfa5f804803f4
Cihat Gündüz <cihat.guenduez@jamitlabs.com>
2018-01-31 17:12:58 +0100

    Implement start of add dependency method

--- beak.swift
+++ beak.swift
@@ -1,4 +1,4 @@
-// beak: kareman/SwiftShell @ .upToNextMajor(from: "4.0.0")
+// beak: kareman/SwiftShell @ .upToNextMajor(from: "4.0.1")
 // beak: sharplet/Regex @ .upToNextMajor(from: "1.1.0")
 // beak: kylef/PathKit @ .upToNextMajor(from: "0.9.0")
 // beak: onevcat/Rainbow @ .upToNextMajor(from: "3.0.3")
@@ -8,32 +8,6 @@ import SwiftShell
 import Regex
 import PathKit
 import Rainbow
-
-// MARK: - Tasks
-/// Initializes the project with the given info.
-public func initialize(projectName: String) throws {
-    try ["README.md", "LICENSE.md", "Logo.png"].forEach { try deleteFile($0) }
-    try renameProject(from: "NewProjectTemplate", to: projectName)
-    try installDependencies()
-}
-
-/// Installs project dependencies.
-public func installDependencies() throws {
-    try installMissingTools([.carthage])
-
-    let command = "carthage bootstrap --platform ios --cache-builds"
-    print("Installing dependencies via Carthage: '\(command)'", level: .info)
-    try runAndPrint(bash: command)
-}
-
-/// Updates project dependencies.
-public func updateDependencies() throws {
-    try installMissingTools([.carthage])
-
-    let command = "carthage update --platform ios --cache-builds"
-    print("Updating dependencies via Carthage: \(command)", level: .info)
-    try runAndPrint(bash: command)
-}
 
 // MARK: - Helpers
 private func deleteFile(_ fileName: String) throws {
@@ -126,3 +100,103 @@ private func installMissingTools(_ tools: [Tool]) throws {
     try tools.filter { $0.isMissing }.forEach { try $0.install() }
 }
 
+private let semanticVersionRegex = Regex("(\\d+)\\.(\\d+)\\.(\\d+)\\s")
+
+private struct SemanticVersion: Comparable, CustomStringConvertible {
+    let major: Int
+    let minor: Int
+    let patch: Int
+
+    init(_ string: String) {
+        guard let captures = semanticVersionRegex.firstMatch(in: string)?.captures else {
+            fatalError("SemanticVersion initializer was used without checking the structure.")
+        }
+
+        major = Int(captures[0]!)!
+        minor = Int(captures[1]!)!
+        patch = Int(captures[2]!)!
+    }
+
+    static func < (lhs: SemanticVersion, rhs: SemanticVersion) -> Bool {
+        return lhs.major < rhs.major || lhs.minor < rhs.minor || lhs.patch < rhs.patch
+    }
+
+    static func == (lhs: SemanticVersion, rhs: SemanticVersion) -> Bool {
+        return lhs.major == rhs.major && lhs.minor == rhs.minor && lhs.patch == rhs.patch
+    }
+
+    var description: String {
+        return "\(major).\(minor)"
+    }
+}
+
+private func appendEntryToCartfile(_ tagline: String, _ githubSubpath: String, _ version: String) throws {
+    let comment = "# \(tagline)\n"
+    let repoSpecifier = "github \"\(githubSubpath)\""
+    let versionSpecifier: String = {
+        guard version != "latest" else {
+            let tagListCommand = "git ls-remote --tags https://github.com/\(githubSubpath).git"
+            let commandOutput = run(bash: tagListCommand).stdout
+            let availableSemanticVersions = semanticVersionRegex.allMatches(in: commandOutput).map { SemanticVersion($0.matchedString) }
+            let latestVersion = availableSemanticVersions.sorted().last!
+            return " ~> \(latestVersion)"
+        }
+
+        return " ~> \(version)"
+    }()
+
+    let textToAddToCartfile = "\n\(comment)\(repoSpecifier)\(versionSpecifier)\n"
+
+    let command = "echo '\(textToAddToCartfile)' >> Cartfile"
+    print("Adding entry to Cartfile with: '\(command)'", level: .info)
+    try runAndPrint(bash: command)
+}
+
+private func synchronizeCarthageAppGroupWithCopyFrameworksBuildPhase() {
+    // TODO: not yet implemented
+}
+
+// MARK: - Tasks
+/// Initializes the project with the given info.
+public func initialize(projectName: String) throws {
+    try ["README.md", "LICENSE.md", "Logo.png"].forEach { try deleteFile($0) }
+    try renameProject(from: "NewProjectTemplate", to: projectName)
+    try installDependencies()
+}
+
+/// Installs project dependencies.
+public func installDependencies() throws {
+    try installMissingTools([.carthage])
+
+    let command = "carthage bootstrap --platform ios --cache-builds"
+    print("Installing dependencies via Carthage: '\(command)'", level: .info)
+    try runAndPrint(bash: command)
+}
+
+/// Updates project dependencies.
+public func updateDependencies() throws {
+    try installMissingTools([.carthage])
+
+    let command = "carthage update --platform ios --cache-builds"
+    print("Updating dependencies via Carthage: \(command)", level: .info)
+    try runAndPrint(bash: command)
+}
+
+
+/// Adds a dependency using the configured package manager.
+public func addDependency(github githubSubpath: String, tagline: String, version: String = "latest") throws {
+    try installMissingTools([.carthage])
+    try appendEntryToCartfile(tagline, githubSubpath, version)
+    try updateDependencies()
+
+    print("Please add the new frameworks to your projects 'Carthage >> App' group in the project navigator, then press any key to continue.", level: .warning)
+    run(bash: "read -n 1 -s")
+
+    print("Synchronizing 'Carthage >> App' group with frameworks in copy build phase.", level: .info)
+    synchronizeCarthageAppGroupWithCopyFrameworksBuildPhase()
+}
+
+/// Adds a testing dependency using the configured package manager.
+public func addTestingDependency(github githubSubpath: String, version: String = "latest") throws {
+    // not yet implemented
+}
