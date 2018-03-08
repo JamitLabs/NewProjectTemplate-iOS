1e05de03ee4b2daae053e6e065258737185f261e
Cihat Gündüz <commits@cihatguenduez.de>
2018-03-08 16:04:10 +0100

    Remove unnecessary header comment from Swift files

--- Sources/Code/Progress.swift
+++ Sources/Code/Progress.swift
@@ -1,7 +1,4 @@
 //
-//  Progress.swift
-//  NewProjectTemplate
-//
 //  Created by Cihat Gündüz on 26.05.17.
 //  Copyright © 2017 Jamit Labs GmbH. All rights reserved.
 //
--- UI Tests/Code/ExampleTestCase.swift
+++ UI Tests/Code/ExampleTestCase.swift
@@ -1,7 +1,4 @@
 //
-//  ExampleTestCase.swift
-//  NewProjectTemplate
-//
 //  Created by Cihat Gündüz on 26.05.17.
 //  Copyright © 2017 Jamit Labs GmbH. All rights reserved.
 //
