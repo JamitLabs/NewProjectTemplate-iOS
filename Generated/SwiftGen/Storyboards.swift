507278818e3bd16563ceb18a4f4e765a6d59f0b6
Cihat Gündüz <cihat.guenduez@jamitlabs.com>
2018-06-21 12:54:48 +0200

    Restructure project with best practices using flat approach in details

--- Generated/SwiftGen/Storyboards.swift
+++ Generated/SwiftGen/Storyboards.swift
@@ -53,11 +53,6 @@ internal extension UIViewController {
 
 // swiftlint:disable explicit_type_interface identifier_name line_length type_body_length type_name
 internal enum StoryboardScene {
-  internal enum LaunchScreen: StoryboardType {
-    internal static let storyboardName = "LaunchScreen"
-
-    internal static let initialScene = InitialSceneType<UIViewController>(storyboard: LaunchScreen.self)
-  }
   internal enum Main: StoryboardType {
     internal static let storyboardName = "Main"
 
