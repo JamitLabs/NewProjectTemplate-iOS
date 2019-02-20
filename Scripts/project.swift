df0f3ecb45ace111f78fd367472796b17ba6a157
Cihat Gündüz <cihat.guenduez@jamitlabs.com>
2019-02-20 15:25:18 +0100

    Write content renaming part of project setup script

--- Scripts/project.swift
+++ Scripts/project.swift
@@ -10,14 +10,45 @@ import SwiftShell
 
 // MARK: - Runnable Tasks
 public func setup(name: String, orga: String) throws {
+    // delete unnecessary files
+    for fileToDelete in ["README.md", "LICENSE", "Logo.png"] {
+        try execute(bash: "rm \(fileToDelete)")
+    }
+
+    // rename sample files to be actual files
+    for sampleFile in ["README.md.sample", "LICENSE.sample"] {
+        try execute(bash: "mv \(sampleFile) \(sampleFile.replacingOccurrences(of: ".sample", with: ""))")
+    }
+
+    // replace name & orga in project file contents
+    try replaceFileContentOccurences(of: "NewProjectTemplate", with: name)
+    try replaceFileContentOccurences(of: "Jamit Labs GmbH", with: orga)
+
+    // rename files with new name
     // TODO: not yet implemented
 
+    // install tools & dependencies
     try execute(bash: "tools install")
     try execute(bash: "deps install")
+
+    // open project in xcode
+    if FileManager.default.fileExists(atPath: "\(name).xcworkspace") {
+        try execute(bash: "open \(name).xcworkspace")
+    } else {
+        try execute(bash: "open \(name).xcodeproj")
+    }
 }
 
 // MARK: - Helpers
 private func execute(bash command: String) throws {
     print("⏳ Executing '\(command.italic.lightYellow)'".bold)
     try runAndPrint(bash: command)
+}
+
+private func replaceFileContentOccurences(of stringToReplace: String, with replacement: String) throws {
+    try execute(bash: "LC_ALL=C find . -d 1 -type f -exec sed -i '' 's/\(stringToReplace)/\(replacement)/g' {} \\;")
+
+    for subfolder in ["App", "Tests", "UITests", "NewProjectTemplate.xcodeproj"] {
+        try execute(bash: "LC_ALL=C find . -regex '\\./\(subfolder)/.*' -type f -exec sed -i '' 's/\(stringToReplace)/\(replacement)/g' {} \\;")
+    }
 }
