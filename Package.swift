5a3adcf66e0326ec88b2c55b32cdeccd552defe8
Cihat Gündüz <cihat.guenduez@jamitlabs.com>
2019-03-08 20:44:48 +0100

    Add dependencies in Package.swift & link in RootFiles group

--- Package.swift
+++ Package.swift
@@ -5,15 +5,34 @@ let package = Package(
     name: "NewProjectTemplate",
     products: [],
     dependencies: [
-        // add your dependencies here, for example:
-        // .package(url: "https://github.com/User/Project.git", .upToNextMajor(from: "1.0.0")),
+        // Handy Swift features that didn't make it into the Swift standard library.
+        .package(url: "https://github.com/Flinesoft/HandySwift.git", .upToNextMajor(from: "2.8.0")),
+
+        // Handy UI features that should have been part of UIKit in the first place.
+        .package(url: "https://github.com/Flinesoft/HandyUIKit.git", .upToNextMajor(from: "1.9.1")),
+
+        // Keep the screen flow and data handling logic out of your view controllers – let them handle view-stuff only.
+        .package(url: "https://github.com/Flinesoft/Imperio.git", .upToNextMajor(from: "3.0.2")),
+
+        // Error Handler based on localized & healable (recoverable) errors without the overhead of NSError.
+        .package(url: "https://github.com/JamitLabs/MungoHealer.git", .upToNextMajor(from: "0.3.2")),
+
+        // Convenient logging during development & release in Swift
+        .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", .upToNextMajor(from: "1.6.2")),
+
+        // Modern Swift API for NSUserDefaults
+        .package(url: "https://github.com/radex/SwiftyUserDefaults.git", .upToNextMajor(from: "4.0.0-beta.1")),
     ],
     targets: [
         .target(
             name: "App",
             dependencies: [
-                // add your dependencies scheme names here, for example:
-                // "Project",
+                "HandySwift",
+                "HandyUIKit",
+                "Imperio",
+                "MungoHealer",
+                "SwiftyBeaver",
+                "SwiftyUserDefaults",
             ]
         ),
     ]
