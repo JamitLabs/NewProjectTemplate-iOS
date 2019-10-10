636ba86cb9f23e71fb6f43cdc232b3b32f3378f4
Frederick Pietschmann <19194800+fredpi@users.noreply.github.com>
2019-10-10 15:18:55 +0200

    Reduce included dependencies

--- Package.swift
+++ Package.swift
@@ -5,44 +5,24 @@ let package = Package(
     name: "NewProjectTemplate",
     products: [],
     dependencies: [
-        // Handy Swift features that didn't make it into the Swift standard library.
-        .package(url: "https://github.com/Flinesoft/HandySwift.git", .upToNextMajor(from: "2.8.0")),
-
-        // Handy UI features that should have been part of UIKit in the first place.
-        .package(url: "https://github.com/Flinesoft/HandyUIKit.git", .upToNextMajor(from: "1.9.1")),
-
         // Keep the screen flow and data handling logic out of your view controllers – let them handle view-stuff only.
         .package(url: "https://github.com/Flinesoft/Imperio.git", .upToNextMajor(from: "3.0.2")),
 
-        // Error Handler based on localized & healable (recoverable) errors without the overhead of NSError.
-        .package(url: "https://github.com/JamitLabs/MungoHealer.git", .upToNextMajor(from: "0.3.2")),
-
-        // 📸 Delightful Swift snapshot testing.
-        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", .upToNextMajor(from: "1.4.0")),
-
         // Convenient logging during development & release in Swift
-        .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", .upToNextMajor(from: "1.7.0")),
-
-        // Modern Swift API for NSUserDefaults
-        .package(url: "https://github.com/radex/SwiftyUserDefaults.git", .upToNextMajor(from: "4.0.0-beta.2")),
+        .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", .upToNextMajor(from: "1.8.2")),
     ],
     targets: [
         .target(
             name: "App",
             dependencies: [
-                "HandySwift",
-                "HandyUIKit",
                 "Imperio",
-                "MungoHealer",
                 "SwiftyBeaver",
-                "SwiftyUserDefaults",
             ],
             path: "App"
         ),
         .testTarget(
             name: "Tests",
             dependencies: [
-                "SnapshotTesting",
             ],
             path: "Tests"
         ),
